53392ed7bdb20e83f6ec5bc2e2b421befa2f9a79
jzheng <jzheng213@gmail.com>
2016-05-02 23:37:54 -0400

    Finished Tutorial for Event Handling and Delegating

---
+++ EventsandDelegates/MessageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsandDelegates
+{
+    public class MessageService
+    {
+        public void OnVideoEncoded(object source, VideoEventArgs arg)
+        {
+            Console.WriteLine("MessageService: Sending a text Messaging..." + arg.Video.Title);
+        }
+    }
+}
